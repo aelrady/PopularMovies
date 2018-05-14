50254f118d38dd58e52bbe06b952f5fccf750787
aelrady <ashley.elrady@gmail.com>
2018-05-14 02:04:32 -0400

    Test commit and push

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
+
     @BindView(R.id.no_connection) TextView connectionTextView;
     @BindView(R.id.network_exception) TextView networkExceptionTextView;
 
