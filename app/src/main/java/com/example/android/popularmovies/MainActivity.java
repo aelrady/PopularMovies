2e6af9156f90c6d5cd01261ec4e010c0b1527288
aelrady <ashley.elrady@gmail.com>
2018-05-14 01:54:28 -0400

    Popular Movies App

---
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.popularmovies.api.MovieApiClient;
+import com.example.android.popularmovies.api.MovieApiInterface;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
+    private String sort_param = "popular";
+    GridLayoutManager gridLayoutManager;
+    private ArrayList<String> picturePathList;
+    private ArrayList<Movie> movies;
+    private MovieAdapter movieAdapter;
+
+    private RecyclerView recyclerView;
+
+    @BindView(R.id.no_connection) TextView connectionTextView;
+    @BindView(R.id.network_exception) TextView networkExceptionTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        networkExceptionTextView.setVisibility(View.GONE);
+
+        if (savedInstanceState == null || !savedInstanceState.containsKey("movies")) {
+            if (isConnected()) {
+                populateGrid();
+                connectionTextView.setVisibility(View.GONE);
+            } else {
+                connectionTextView.setVisibility(View.VISIBLE);
+            }
+        } else {
+            movies = savedInstanceState.getParcelableArrayList("movies");
+            picturePathList = savedInstanceState.getStringArrayList("pictures");
+            recyclerView = findViewById(R.id.rv_movies);
+            gridLayoutManager = new GridLayoutManager(this, 2);
+            recyclerView.setLayoutManager(gridLayoutManager);
+            movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
+            recyclerView.setAdapter(movieAdapter);
+            connectionTextView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("movies", movies);
+        outState.putStringArrayList("pictures", picturePathList);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sort_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (isConnected()) {
+            connectionTextView.setVisibility(View.GONE);
+            if (item.getItemId() == R.id.most_popular) {
+                sort_param = "popular";
+                populateGrid();
+            } else if (item.getItemId() == R.id.highest_rated) {
+                sort_param = "top_rated";
+                populateGrid();
+            } else {
+                return super.onOptionsItemSelected(item);
+            }
+        } else {
+            connectionTextView.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+
+    public void populateGrid() {
+
+        final String your_api_key = "";
+
+        recyclerView = findViewById(R.id.rv_movies);
+        gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        MovieApiInterface movieApiInterface = MovieApiClient.getClient().create(MovieApiInterface.class);
+
+        Call<MovieResults> call = movieApiInterface.getMovies(sort_param, your_api_key);
+        call.enqueue(new Callback<MovieResults>() {
+            @Override
+            public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
+                movies = (ArrayList<Movie>) response.body().getResults();
+                picturePathList = new ArrayList<>();
+                for (int i = 0; i < movies.size(); i++) {
+                    picturePathList.add(IMAGE_BASE_URL + movies.get(i).getPosterPath());
+                }
+                movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
+                recyclerView.setAdapter(movieAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<MovieResults> call, Throwable t) {
+                networkExceptionTextView.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+    };
+}
---
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -0,0 +1,69 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+    private ArrayList<Movie> mMovies;
+    private ArrayList<String> mMoviePosters;
+    private Context mContext;
+
+    public MovieAdapter(Context context, ArrayList<String> moviePosters, ArrayList<Movie> movies) {
+        this.mContext = context;
+        this.mMoviePosters = moviePosters;
+        this.mMovies = movies;
+    }
+
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView mMovieImageView;
+        RelativeLayout relativeLayout;
+
+        public MovieAdapterViewHolder(View itemView) {
+            super(itemView);
+            mMovieImageView = itemView.findViewById(R.id.poster_image);
+            relativeLayout = itemView.findViewById(R.id.grid_layout);
+        }
+    }
+
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForGridItem = R.layout.grid_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(layoutIdForGridItem, viewGroup, false);
+        return new MovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieAdapterViewHolder holder, final int position) {
+        String moviePoster = mMoviePosters.get(position);
+        Picasso.with(mContext).load(moviePoster).into(holder.mMovieImageView);
+
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra("movie", mMovies.get(position));
+                mContext.startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMoviePosters.size();
+    }
+}
