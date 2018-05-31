f86119b6edcf0c9e8780a09999f7a35a05ea6a68
aelrady <ashley.elrady@gmail.com>
2018-05-31 00:18:11 -0400

    Display movie reviews in ReviewActivity

--- app/src/main/java/com/example/android/popularmovies/ReviewActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ReviewActivity.java
@@ -3,7 +3,9 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.android.popularmovies.model.Review;
 
@@ -12,6 +14,11 @@ import java.util.ArrayList;
 public class ReviewActivity extends AppCompatActivity {
 
     private ArrayList<Review> reviews;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
+    private ReviewAdapter reviewAdapter;
+    private ArrayList<String> authorList;
+    private ArrayList<String> reviewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,23 @@ public class ReviewActivity extends AppCompatActivity {
     private void populateReviewActivity() {
         Intent intent = getIntent();
         reviews = intent.getParcelableArrayListExtra("reviews");
-        Log.v("Show More Reviews: ", String.valueOf(reviews));
+
+        authorList = new ArrayList<>();
+        for (int i = 0; i < reviews.size(); i++) {
+            authorList.add(reviews.get(i).getAuthor());
+        }
+
+        reviewList = new ArrayList<>();
+        for (int i = 0; i < reviews.size(); i++) {
+            reviewList.add(reviews.get(i).getContent());
+        }
+
+        recyclerView = findViewById(R.id.rv_reviews);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        reviewAdapter = new ReviewAdapter(ReviewActivity.this, authorList, reviewList);
+        recyclerView.setAdapter(reviewAdapter);
+        DividerItemDecoration itemDecor = new DividerItemDecoration(ReviewActivity.this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecor);
     }
 }
